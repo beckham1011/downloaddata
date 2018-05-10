d3acb34b383556a4cc31ae0104c6e5332b2fa611
BeckXu0089124 <Zhijie.Xu@ge.com>
2018-05-11 06:55:46 +0800

    TestCommit

--- src/main/java/com/beck/download/boot/Application.java
+++ src/main/java/com/beck/download/boot/Application.java
@@ -25,6 +25,7 @@ public class Application {
 		TomcatEmbeddedServletContainerFactory tomcatFactory = new TomcatEmbeddedServletContainerFactory();  
         tomcatFactory.addConnectorCustomizers(new CCMTomcatConnectorCustomizer());  
 		System.out.println("Test");
+		
 		return tomcatFactory;
 	}
 }
